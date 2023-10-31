a2b7b6da2c2368555804a92051869ba988d993c0
0991951659 <palinsupradit@gmail.com>
2023-10-31 19:13:35 +0000

    Updated the second argument to the hundred function

--- hundred.R
+++ hundred.R
@@ -14,6 +14,39 @@ hundred <- function(data) {
 }
 
 
+# Update the function by the adding the operation type as a second argument 
+hundred <- function(data, operation_type) {
+  hundred <- function(data, operation_type) {
+    # Check if the input is a data frame
+    if (!is.data.frame(data)) {
+      stop("Input data must be a data frame.")
+    }
+    
+    # Identify numeric columns
+    numeric_cols <- sapply(data, is.numeric)
+    
+    if (operation_type == "multiply") {
+      # Multiply only the numeric columns by 100
+      data[numeric_cols] <- data[numeric_cols] * 100
+    } else if (operation_type == "sum") {
+      # Add 100 to the numeric columns
+      data[numeric_cols] <- data[numeric_cols] + 100
+    } else if (operation_type == "subtract") {
+      # Subtract 100 from the numeric columns
+      data[numeric_cols] <- data[numeric_cols] - 100
+    } else if (operation_type == "divide") {
+      # Divide the numeric columns by 100
+      data[numeric_cols] <- data[numeric_cols] / 100
+    } else {
+      stop("Invalid operation type.")
+    }
+    
+    return(data)
+  }
+  
+  # adding multiply operation as the second argument for the iris dataset
+  hundred(iris, "multiply")
+
 # Test with the "iris" data: a built-in R dataset with four numeric variables containing 
 # measurements of 4 different attributes for 50 flowers 
 data("iris")
